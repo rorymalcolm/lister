d8534f57d599850c3f0e0d573a9d90f8437fe824
Rory Malcolm <RoryMcKenzieMalcolm@Gmail.com>
2015-03-15 15:41:13 +0000

    file arch and sql load

---
+++ app/includes/ajaxloadposts.php
@@ -0,0 +1,19 @@
+<?php
+$db = new PDO('mysql:host=localhost;dbname=listar;charset=utf8', 'root', '');
+try{
+    foreach($db->query('SELECT * FROM tracks ORDER BY karma DESC') as $row){
+        echo '<li class = "trackwrap">';
+        echo '<div class = "scorewrap">';
+        echo '<p class = "trackscore">'.$row['karma'].' points</p>';
+        echo '<button class = "upvote" type="button">+</button>';
+        echo '<button class = "downvote" type="button">-</button>';
+        echo '</div>';
+        echo '<a  class = "tracklink" href="'.$row['url'].'">'.$row['title'].'</a>';
+        echo '</li>';
+    }
+} 
+catch(PDOException $ex) {
+    echo "An Error occured!";
+    some_logging_function($ex->getMessage());
+}
+?>
