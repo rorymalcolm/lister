6fd7a43dffb91a44e8a1787bd8a7c0521581ce6e
chrispollock <29chrispy@gmail.com>
2015-08-18 12:26:29 +0100

    changed ajaxloadposts.php to make it readable

--- app/includes/ajaxloadposts.php
+++ app/includes/ajaxloadposts.php
@@ -1,21 +1,31 @@
 <?php
 $playlist_id = 0;
 $db = new PDO('mysql:host=localhost;dbname=listar;charset=utf8', 'root', 'root');
-
 try{
-    foreach($db->query('SELECT * FROM tracks WHERE playlist_id = '.$playlist_id.' ORDER BY karma DESC') as $row){
-        echo '<li id="'.$row['id'].'" class = "trackwrap">';
-        echo    '<div class = "scorewrap">';
-        echo        '<div class = "trackscore">'.$row['karma'].' points</div>';
-        echo        '<button class = "upvote" type="button">+</button>';
-        echo        '<button class = "downvote" type="button">-</button>';
-        echo        '<button class = "deletebutt" type="button">delete</button>';
-        echo    '</div>';
-        echo    '<a  class = "tracklink" href="'.$row['url'].'">'.$row['title'].'</a>';
-        echo '</li>';
+
+$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+
+$result = mysql_query('SELECT * FROM `tracks` WHERE `playlist_id` = '.$playlist_id.'');
+
+if (!$result) {
+    echo 'list does not exist';
+}
+    
+    foreach($db->query('SELECT * FROM tracks WHERE playlist_id = '.$playlist_id.' ORDER BY karma DESC') as $row){ 
+    ?>
+        <li id="<? echo $row['id'] ?>" class = "trackwrap">
+            <div class = "scorewrap">
+                <div class = "trackscore"><? echo $row['karma'] ?> points</div>
+                <button class = "upvote" type="button">+</button>
+                <button class = "downvote" type="button">-</button>
+                <button class = "deletebutt" type="button">delete</button>
+            </div>
+            <a  class = "tracklink" href="<? echo $row['url'] ?>"><? echo $row['title'] ?></a>
+        </li>
+    <?php
     }
     if (empty($row)){
-      echo '<li id="emptylistmessage"><p>list is empty :(</p><p>add some stuff above</p></li>';
+      ?> <li id="emptylistmessage"><p>list is empty :(</p><p>add some stuff above</p></li> <?php
     }
 }
 catch(PDOException $ex) {
